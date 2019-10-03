ae84c416ff9ee9f08de3f377bd3af786f791c588
vitfolv <55414076+vitfolv@users.noreply.github.com>
2019-10-04 00:24:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -70,8 +70,6 @@ def predefined_commands(message):
 	if message.from_user.id in all_students.values():
 		if "случайный мем" in message.text or "рандомный мем" in message.text or "дай мем" in message.text or "meme" in message.text:
 			bot.send_photo(message.chat.id, meme_url)
-	else:
-		bot.send_message(message.chat.id, "ты мне не нравишься", reply_to_message_id = message.message_id)
 	if weekorder == True:
 		if date.today().weekday() == 0 and "какой" in message.text and "день" in message.text:
 			bot.send_message(message.chat.id, "сегодня светлый " + today, reply_to_message_id = message.message_id)
