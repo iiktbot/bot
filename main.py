60350d3236123e75170962912dc5dc9a691ac5cc
vitfolv <55414076+vitfolv@users.noreply.github.com>
2019-10-14 07:11:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1070,10 +1070,10 @@ def predefined_messages(message):
                 bot.send_chat_action(mcid, "typing")
                 time.sleep(0.3)
                 bot.send_message(mcid, "вряд ли ты здесь учишься", reply_to_message_id=message.message_id)
-if "бот" in msg and not any(words in msg for words in messages_tuple):
-    ai_mode = 1
-else:
-    ai_mode = 0
+    if "бот" in msg and not any(words in msg for words in messages_tuple):
+        ai_mode = 1
+    else:
+        ai_mode = 0
 
 def ai_message(bot, update):
     if ai_mode == 1:
