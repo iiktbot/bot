6515dab8bce769a90a654925cadc5695b84847a4
vitfolv <55414076+vitfolv@users.noreply.github.com>
2019-10-19 04:50:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -690,4 +690,4 @@ def process_webhook():
     return "", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
