9f1cbf89273e27c7394c48cffb04f7648569c105
vitfolv <55414076+vitfolv@users.noreply.github.com>
2019-10-29 02:46:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def predefined_messages(message):
     date_week = date.today().isocalendar()[1]
     date_weekday = date.today().weekday()
     time_now = datetime.time(23, 59, 59) #datetime.datetime.now() + datetime.timedelta(hours=18)
-    time_day_beg = datetime.time(00, 00, 01)
+    time_day_beg = datetime.time(00, 00, 1)
     time_uni_end = datetime.time(15, 50, 00)
     time_day_end = datetime.time(00, 00, 00)
 
