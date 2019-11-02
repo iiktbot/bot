14da60877b3489e57f2747fe51d880b3d7359578
vitfolv <55414076+vitfolv@users.noreply.github.com>
2019-11-02 18:01:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -495,9 +495,9 @@ def schedule_message(message):
 
     meme_tomorrow = datetime.datetime.replace(now + delta)
     meme_delta = meme_tomorrow - now
-    meme_last_num = 280
+    meme_last_num = 0
 
-    if time_now == time_day_beg:
+    while meme_last_num > 0:
         time.sleep(meme_delta.seconds)
         meme_last_num += 3
 
