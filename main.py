76fefbfe665eae66f30f97d436c68871a7149738
vitfolv <55414076+vitfolv@users.noreply.github.com>
2019-11-05 18:32:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -573,9 +573,7 @@ def predefined_messages(message):
 			else:
 				days_condition = 'not ok'
 		elif 'завтра' in msg:
-			if 0 < weekdays_matches < 2 and msg.count('вт') == 1:
-				days_condition = 'ok'
-			elif any(word in msg for word in tomorrow_list) and not any(word in msg for word in tomorrow_unnecessary_list) and msg.count('вт') >= 1:
+			if msg.count('вт') >= 1 and any(word in msg for word in tomorrow_list) and not any(word in msg for word in tomorrow_unnecessary_list):
 				days_condition = 'ok'
 			else:
 				days_condition = 'not ok'
