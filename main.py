62c81996da2f1387f5408864ff839090612ec909
vitfolv <55414076+vitfolv@users.noreply.github.com>
2019-11-05 17:47:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -573,12 +573,20 @@ def predefined_messages(message):
 			else:
 				days_condition = 'not ok'
 		elif 'завтра' in msg:
-			if 0 < weekdays_matches < 2 and msg.count('вт') == 1:
-				days_condition = 'ok'
-			elif any(word in msg for word in tomorrow_list) and not any(word in msg for word in tomorrow_unnecessary_list):
-				days_condition = 'ok'
+			if tomorrow == 'вторник':
+				if 0 < weekdays_matches < 2 and msg.count('вт') == 1:
+					days_condition = 'ok'
+				elif any(word in msg for word in tomorrow_list) and not any(word in msg for word in tomorrow_unnecessary_list) and msg.count('вт') >= 1:
+					days_condition = 'ok'
+				else:
+					days_condition = 'not ok'
 			else:
-				days_condition = 'not ok'
+				if not any(word in msg for word in weekdays_list):
+					days_condition = 'ok'
+				elif any(word in msg for word in tomorrow_list) and not any(word in msg for word in tomorrow_unnecessary_list):
+					days_condition = 'ok'
+				else:
+					days_condition = 'not ok'
 		elif 'вчера' in msg:
 			if not any(word in msg for word in weekdays_list):
 				days_condition = 'ok'
