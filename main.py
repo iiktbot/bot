8460752447c0bf63470c9272470d6f4aee99e9e7
vitfolv <55414076+vitfolv@users.noreply.github.com>
2019-11-11 05:20:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -288,9 +288,9 @@ def start_message(message):
 	mct = message.chat.type
 
 	if uid in first_group.keys():
-		student_name = ', ' + first_group[uid].split(' ', 1)[0]
+		student_name = ', ' + '[' + first_group[uid].split(' ', 1)[0] + '](tg://user?id=' + str(uid) + ')'
 	elif uid in second_group.keys():
-		student_name = ', ' + second_group[uid].split(' ', 1)[0]
+		student_name = ', ' + '[' + second_group[uid].split(' ', 1)[0] + '](tg://user?id=' + str(uid) + ')'
 	else:
 		student_name = ''
 
@@ -304,13 +304,6 @@ def classes_command(message):
 	cid = message.chat.id
 	uid = message.from_user.id
 	mct = message.chat.type
-
-	if uid in first_group.keys():
-		student_name = '[' + first_group[uid].split(' ', 1)[0] + '](tg://user?id=' + str(uid) + ')' + ', '
-	elif uid in second_group.keys():
-		student_name = '[' + second_group[uid].split(' ', 1)[0] + '](tg://user?id=' + str(uid) + ')' + ', '
-	else:
-		student_name = ''
 
 	board_add = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
 	board_add.row(types.KeyboardButton('вчера'), types.KeyboardButton('сегодня'), types.KeyboardButton('завтра'))
