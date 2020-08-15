77c078e6c9992a0099c6028db14c5590055924af
iiktbot <55414076+iiktbot@users.noreply.github.com>
2020-08-15 09:25:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ TOKEN = os.environ['TOKEN']
 HOST = os.environ['HOST']
 PORT = os.environ['PORT']
 URL = os.environ['URL']
+
 bot = telegram.Bot(TOKEN)
 app = Flask(__name__)
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
 logging.basicConfig(
     level=logging.DEBUG, format='%(levelname)s: %(name)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -24,8 +28,6 @@ def start_cmd(update, context):
 
 def main():
     start_cmd_handler = CommandHandler('start', start_cmd)
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
     dp.add_handler(start_cmd_handler)
     updater.start_webhook(listen=HOST, port=PORT, url_path=TOKEN)
     updater.bot.set_webhook(URL + TOKEN)
@@ -40,8 +42,8 @@ def index():
 @app.route('/' + TOKEN, methods=['POST'])
 def process_webhook():
     update = telegram.update.Update.de_json(request.get_json(force=True), bot)
-    print(update)
-    return 'ok'
+    dp.process_update(update)
+    return "<p>that's ok</p>"
 
 
 if __name__ == "__main__":
